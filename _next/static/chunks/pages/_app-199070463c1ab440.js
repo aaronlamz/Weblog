4eae27e8e3169b80b7d0af7d7b65c4212a54ffd8
aaronlamz <aaronlamz@users.noreply.github.com>
2025-08-14 13:08:24 +0000

    deploy: 78d5d14d4359248f81e98ec75f006fd0a3aab7bc

---
+++ _next/static/chunks/pages/_app-199070463c1ab440.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{2379:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(4864)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(2379),n(1518))),_N_E=_.O()}]);
